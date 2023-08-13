50abd3f135f964bf4f839d9fbb3d1cb8ef86c8a2
Ridwan <ridwan123.official@gmail.com>
2023-08-13 23:09:25 +0600

    1107 added

---
+++ medium/1107-New Users Daily Count.sql
@@ -0,0 +1,7 @@
+with cte as(select user_id,min(activity_date) as first_login from traffic_1107
+where activity = 'login' 
+group by user_id
+having min(activity_date) between '2019-03-30' and '2019-06-30')
+
+select first_login as login_date,count(*) as user_count  from cte
+group by first_login
