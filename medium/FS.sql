746ddebd6c581799dfc2d94bcb2a7d2611f603b7
Ridwan <ridwan123.official@gmail.com>
2023-08-14 00:37:47 +0600

    added

---
+++ medium/FS.sql
@@ -0,0 +1,31 @@
+with cte as(
+    select
+        action_date,
+        ROUND(
+            (
+                SUM(
+                    CASE
+                        WHEN remove_date is not null THEN 1
+                        ELSE 0
+                    END
+                )
+            ),
+            0
+        ) as DAILY_post_removed,
+        COUNT(*) AS DAILY_TOTAL_SPAM_REPORTED
+    from
+        actions a
+        left join removals_1132 r on a.post_id = r.post_id
+    where
+        action = 'report'
+        and extra = 'spam'
+    group by
+        action_date
+)
+SELECT
+    ROUND(
+        AVG(DAILY_post_removed / DAILY_TOTAL_SPAM_REPORTED) * 100,
+        2
+    ) AS average_daily_percent
+FROM
+    CTE
