2b048f6065f30f221eaf6b428aeb26a1ff72ac01
Ridwan <ridwan123.official@gmail.com>
2023-08-14 23:11:52 +0600

    added

---
+++ medium/2159-Order Two Columns Independently.sql
@@ -0,0 +1,20 @@
+with cte as(
+    SELECT
+        *,
+        row_number() over(
+            order by
+                first_col
+        ) as f_rnk,
+        row_number() over(
+            order by
+                second_col desc
+        ) as s_rnk
+    FROM
+        data_2159
+)
+select
+    f.first_col,
+    s.second_col
+from
+    cte f
+    join cte s on f.f_rnk = s.s_rnk
