e2a9ef9becf2e9a5e2f73a403e1b392cc9490821
Ridwan <ridwan123.official@gmail.com>
2023-08-14 21:25:10 +0600

    added

---
+++ medium/1264-Page Recommendations.sql
@@ -0,0 +1,50 @@
+with cte as (
+    select
+        *
+    from
+        friendship_1264
+    where
+        user_id1 = 1
+        or user_id2 = 1
+),
+cte1 as (
+    select
+        user_id1 as u1frnd
+    from
+        cte
+    where
+        user_id1 <> 1
+    union
+    select
+        user_id2 as u1frnd
+    from
+        cte
+    where
+        user_id2 <> 1
+),
+cte2 as (
+    select
+        page_id
+    from
+        likes_1264
+    where
+        user_id in (
+            select
+                *
+            from
+                cte1
+        )
+    except
+    select
+        page_id
+    from
+        likes_1264
+    where
+        user_id = 1
+    order by
+        page_id
+)
+select
+    page_id as recommended_page
+from
+    cte2
