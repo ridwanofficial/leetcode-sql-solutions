900482902090fa9ac46aa64632bc5fce5c93ea8f
Ridwan <ridwan123.official@gmail.com>
2023-08-14 22:24:03 +0600

    added

---
+++ medium/612-Shortest Distance in a Plane.sql
@@ -0,0 +1,19 @@
+with cte as(
+    select
+        *,
+        row_number() over() as serial
+    from
+        point_2d_612
+)
+select
+    ROUND(
+        SQRT(POW(p1.x - p2.x, 2) + POW(p1.y - p2.y, 2)) :: numeric,
+        2
+    ) as shortest
+from
+    cte p1
+    join cte p2 on p1.serial > p2.serial
+order by
+    shortest_distance
+limit
+    1
