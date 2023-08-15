8192a8c897526e3400093dbdd855bb9adc95e56b
Ridwan <ridwan123.official@gmail.com>
2023-08-15 16:44:47 +0600

    added

---
+++ medium/2388-Change Null Values in a Table to the Previous Value.sql
@@ -0,0 +1,27 @@
+with cte as (
+    select
+        *,
+        row_number() over() as rnk
+    from
+        Coffee_Shop_2388
+),
+cte2 as (
+    select
+        *,
+        SUM(
+            CASE
+                WHEN drink IS NOT NULL THEN 1
+                ELSE 0
+            END
+        ) OVER (
+            ORDER BY
+                rnk
+        ) AS group_id
+    from
+        cte
+)
+select
+    id,
+    First_value(drink) over(partition by group_id) as drink
+from
+    cte2
