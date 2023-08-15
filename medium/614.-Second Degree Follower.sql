3b52432ed73bef0d66a5c0dd7b1d6cd02cbdc17a
Ridwan <ridwan123.official@gmail.com>
2023-08-16 02:25:34 +0600

    added

---
+++ medium/614.-Second Degree Follower.sql
@@ -0,0 +1,25 @@
+with cte as(
+    select
+        followee,
+        count(*) as num
+    from
+        Follow
+    group by
+        followee
+),
+cte1 as (
+    select
+        distinct follower
+    from
+        Follow
+)
+select
+    follower,
+    num
+from
+    cte
+    left join cte1 on cte.followee = cte1.follower
+where
+    follower is not null
+order by
+    follower
