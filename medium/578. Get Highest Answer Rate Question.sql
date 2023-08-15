571109c63df4b06ecf00c59e8f2e32f8ea1735fd
Ridwan <ridwan123.official@gmail.com>
2023-08-16 01:15:19 +0600

    added

---
+++ medium/578. Get Highest Answer Rate Question.sql
@@ -0,0 +1,20 @@
+select
+    question_id
+from
+    SurveyLog
+group by
+    question_id
+order by
+    sum(
+        case
+            when action = 'answer' then 1
+            else 0
+        end
+    ) / sum(
+        case
+            when action = 'show' then 1
+            else 0
+        end
+    ) desc
+limit
+    1
