5716d21fb3ca231dcf9c3bad84d0ee73cd078ee3
Ridwan <ridwan123.official@gmail.com>
2023-08-15 20:28:49 +0600

    added

---
+++ medium/1501-Countries You Can Safely Invest In.sql
@@ -0,0 +1,58 @@
+with parsedCountry as(
+    select
+        *,
+        substring(
+            phone_number
+            from
+                '(\d+)'
+        ) AS country_code
+    from
+        person
+),
+cte as (
+    select
+        id,
+        country_code,
+        c.name
+    from
+        parsedCountry p
+        join country c using(country_code)
+),
+cte1 as (
+    select
+        caller_id as id,
+        duration
+    from
+        calls
+    union
+    all
+    select
+        callee_id as id,
+        duration
+    from
+        calls
+),
+cte2 as(
+    select
+        id,
+        round(avg(duration), 5) as avg_duration
+    from
+        cte1
+    group by
+        id
+),
+cte3 as (
+    select
+        name,
+        round(avg(avg_duration) over(partition by name), 5) as duration,
+        round(avg(avg_duration) over(), 5) as global_duration
+    from
+        cte2 c2
+        join cte c using(id)
+)
+select
+    distinct name
+from
+    cte3
+where
+    global_duration < duration
