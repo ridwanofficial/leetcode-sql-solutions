4a8dbb3ff41d0dc139b05052eb4289371f90de1b
Ridwan <ridwan123.official@gmail.com>
2023-08-15 17:43:15 +0600

    added

---
+++ medium/1555-Bank Account Summary.sql
@@ -0,0 +1,34 @@
+with cte as (
+    select
+        paid_by as user_id,
+        - amount as credit
+    from
+        Transactions
+    union
+    all
+    select
+        paid_to as user_id,
+        amount as credit
+    from
+        Transactions
+),
+cte1 as(
+    select
+        user_id,
+        sum(credit) as credit
+    from
+        cte
+    group by
+        user_id
+)
+select
+    u.user_id,
+    u.user_name,
+    (coalesce(c.credit, 0) + u.credit) as credit,
+    case
+        when (c.credit + u.credit) < 0 then 'Yes'
+        ELSE 'No'
+    end as credit_limit_breached
+from
+    cte1 c
+    right join Users u using(user_id)
