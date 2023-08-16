693494078be25341a1c3ca9bfa3a6b4753ef8f07
Ridwan <ridwan123.official@gmail.com>
2023-08-16 22:30:10 +0600

    added

---
+++ hard/1412-Find the Quiet Students in All Exams.sql
@@ -0,0 +1,33 @@
+with cte as (
+    select
+        *,
+        max(score) over() as high,
+        min(score) over() as low
+    from
+        students
+        join exams using(student_id)
+)
+select
+    distinct student_id,
+    student_name
+from
+    cte
+except
+select
+    distinct student_id,
+    student_name
+from
+    cte
+where
+    score = (
+        select
+            max(score)
+        from
+            exams
+    )
+    or score = (
+        select
+            min(score)
+        from
+            exams
+    )
