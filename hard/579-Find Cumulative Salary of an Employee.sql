57c2fea7584723b1ef33c6f7e8bce4df414a8e37
Ridwan <ridwan123.official@gmail.com>
2023-08-19 02:56:14 +0600

    added

--- hard/579-Find Cumulative Salary of an Employee.sql
+++ hard/579-Find Cumulative Salary of an Employee.sql
@@ -9,8 +9,8 @@ with cte as (
                 e.month desc
         ) as rnk
     from
-        Employee_579 e
-        join Employee_579 e1 on e.id = e1.id
+        Employee e
+        join Employee e1 on e.id = e1.id
         and e.month - e1.month between 0
         and 2
 ),
@@ -33,4 +33,46 @@ group by
     month
 order by
     id,
-    month desc
+    month desc -------------------------------------- ------------------- -------------------
+    -------------------------------------- ------------------- -------------------
+    -------------------------------------- ------------------- -------------------
+    ---------------------------solution 2-----------------------
+    -------------
+    with cte5 as(
+        select
+            *,
+            rank() over(
+                partition by id
+                order by
+                    month desc
+            ) as rnk
+        from
+            Employee
+    ),
+    cte1 as(
+        select
+            *
+        from
+            cte5
+        where
+            rnk <> 1
+    )
+select
+    e.id as id,
+    e.month as single_month,
+    (
+        coalesce(e1.salary, 0) + coalesce(e.salary, 0) + coalesce(e2.salary, 0)
+    ) as cumulative_salary
+from
+    cte1 e
+    left join cte1 e1 on (
+        e.id = e1.id
+        and e.month = e1.month + 1
+    )
+    left join cte1 e2 on (
+        e1.id = e2.id
+        and e1.month = e2.month + 1
+    )
+order by
+    e.id,
+    e.month desc
