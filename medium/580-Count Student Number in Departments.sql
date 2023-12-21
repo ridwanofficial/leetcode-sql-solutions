fe68dffbc575571325ebd39bc9201be5914b2e2c
Ridwan <ridwan123.official@gmail.com>
2023-12-21 22:02:48 +0600

    580 updated

--- medium/580-Count Student Number in Departments.sql
+++ medium/580-Count Student Number in Departments.sql
@@ -14,3 +14,28 @@ select
 from
     cte c
     right join department d on c.dept_id = d.dept_id
+    ------------ Alternative
+with
+    cte as (
+        select
+            dept_name,
+            SUM(
+                CASE
+                    WHEN student_id IS NULL THEN 0
+                    ELSE 1
+                END
+            ) as student_number -- COUNT(student_id) AS student_number
+        from
+            student_580
+            right join department_580 using (dept_id)
+        group by
+            dept_id,
+            dept_name
+    )
+select
+    *
+from
+    cte
+order by
+    student_number desc,
+    dept_name
