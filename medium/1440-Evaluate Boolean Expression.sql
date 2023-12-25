b32355868bc78d33275a8455b6044b531111732b
Ridwan <ridwan123.official@gmail.com>
2023-12-25 22:32:25 +0600

    a

--- medium/1440-Evaluate Boolean Expression.sql
+++ medium/1440-Evaluate Boolean Expression.sql
@@ -26,19 +26,34 @@ SELECT
     operator,
     right_operand,
     CASE
-        WHEN operator = '>' THEN CASE
-            WHEN left_val > right_val THEN 'true'
-            ELSE 'false'
-        END
-        WHEN operator = '<' THEN CASE
-            WHEN left_val < right_val THEN 'true'
-            ELSE 'false'
-        END
-        WHEN operator = '=' THEN CASE
-            WHEN left_val = right_val THEN 'true'
-            ELSE 'false'
-        END
+        WHEN operator = '>'
+        AND left_val > right_val THEN 'true'
+        WHEN operator = '<'
+        and left_val < right_val THEN 'true'
+        WHEN operator = '='
+        AND left_val = right_val THEN 'true'
         ELSE 'false'
     END AS value
 FROM
     cte;
+
+---alternative  
+SELECT
+    e.left_operand,
+    e.operator,
+    e.right_operand,
+    (
+        CASE
+            WHEN e.operator = '<'
+            AND v1.value < v2.value THEN 'true'
+            WHEN e.operator = '='
+            AND v1.value = v2.value THEN 'true'
+            WHEN e.operator = '>'
+            AND v1.value > v2.value THEN 'true'
+            ELSE 'false'
+        END
+    ) AS value
+FROM
+    Expressions e
+    JOIN Variables v1 ON e.left_operand = v1.name
+    JOIN Variables v2 ON e.right_operand = v2.name
